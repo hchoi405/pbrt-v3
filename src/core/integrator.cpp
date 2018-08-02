468a62bbc7fcc8cfb9637ecdd316eec9174c828e
Hajin Choi <xzvfinet@gmail.com>
2018-08-02 13:23:34 +0900

    Split adpative sampling function from original

--- src/core/integrator.cpp
+++ src/core/integrator.cpp
@@ -304,6 +304,7 @@ std::vector<Spectrum> SamplerIntegrator::processPixel(
     return radianceValues;
 }
 
+#ifdef ADAPTIVE_SAMPLING
 // SamplerIntegrator Method Definitions
 void SamplerIntegrator::Render(const Scene &scene) {
     Preprocess(scene, *sampler);
@@ -446,7 +447,6 @@ void SamplerIntegrator::Render(const Scene &scene) {
                             tileSampler, arena, filmTileArray[pixelIndex]);
                         localTime = std::clock() - localStart;
 
-#ifdef ADAPTIVE_SAMPLING
                         // mean and variance of this batch
                         Spectrum sMean = std::accumulate(radianceValues.begin(),
                                                          radianceValues.end(),
@@ -482,15 +482,12 @@ void SamplerIntegrator::Render(const Scene &scene) {
                             break;
                         }
 
-#endif
-
                         // counter[pixelIndex] = std::clock() - start;
                     },
                     efficiencyList.size());
 
                 overheadStart = clock();
 
-#ifdef ADAPTIVE_SAMPLING
                 // do not sort at last iteration
                 if (batch == BATCH_NUM) {
                     break;
@@ -550,19 +547,11 @@ void SamplerIntegrator::Render(const Scene &scene) {
                                      efficiencyList[ind].pixel.x]++;
                 }
 
-#else
-                for (size_t i = 0; i < remainingSamples.size(); ++i) {
-                    auto &pEff = efficiencyList[i];
-                    remainingSamples[pEff.pixel.y * imageX + pEff.pixel.x] =
-                        BATCH_SIZE;
-                }
-#endif
                 overheadTime += clock() - overheadStart;
             }
             // reporter.Done();
         }
 
-#ifdef ADAPTIVE_SAMPLING
         // [Stats] ==================================================
         globalTime = std::clock() - globalStart - overheadTime;
         printf("------------[Statistics]------------\n");
@@ -609,7 +598,6 @@ void SamplerIntegrator::Render(const Scene &scene) {
                    0);
         writeImage(path, "efficiency.exr", efficiencyMap, Point2i(256, 256), 0);
         writeImage(path, "time.exr", timeMap, Point2i(256, 256), 0);
-#endif
 
         // Merge image tile into _Film_
         for (auto &filmTile : filmTileArray) {
@@ -629,6 +617,126 @@ void SamplerIntegrator::Render(const Scene &scene) {
     std::cout << "Rendering finished" << std::endl;
     LOG(INFO) << "Rendering finished";
 }
+#else
+// SamplerIntegrator Method Definitions
+void SamplerIntegrator::Render(const Scene &scene) {
+    Preprocess(scene, *sampler);
+    // Render image tiles in parallel
+
+    // Compute number of tiles, _nTiles_, to use for parallel rendering
+    Bounds2i sampleBounds = camera->film->GetSampleBounds();
+    Vector2i sampleExtent = sampleBounds.Diagonal();
+    const int tileSize = 16;
+    Point2i nTiles((sampleExtent.x + tileSize - 1) / tileSize,
+                   (sampleExtent.y + tileSize - 1) / tileSize);
+    ProgressReporter reporter(nTiles.x * nTiles.y, "Rendering");
+    {
+        ParallelFor2D(
+            [&](Point2i tile) {
+                // Render section of image corresponding to _tile_
+
+                // Allocate _MemoryArena_ for tile
+                MemoryArena arena;
+
+                // Get sampler instance for tile
+                int seed = tile.y * nTiles.x + tile.x;
+                std::unique_ptr<Sampler> tileSampler = sampler->Clone(seed);
+
+                // Compute sample bounds for tile
+                int x0 = sampleBounds.pMin.x + tile.x * tileSize;
+                int x1 = std::min(x0 + tileSize, sampleBounds.pMax.x);
+                int y0 = sampleBounds.pMin.y + tile.y * tileSize;
+                int y1 = std::min(y0 + tileSize, sampleBounds.pMax.y);
+                Bounds2i tileBounds(Point2i(x0, y0), Point2i(x1, y1));
+                LOG(INFO) << "Starting image tile " << tileBounds;
+
+                // Get _FilmTile_ for tile
+                std::unique_ptr<FilmTile> filmTile =
+                    camera->film->GetFilmTile(tileBounds);
+
+                // Loop over pixels in tile to render them
+                for (Point2i pixel : tileBounds) {
+                    {
+                        ProfilePhase pp(Prof::StartPixel);
+                        tileSampler->StartPixel(pixel);
+                    }
+
+                    // Do this check after the StartPixel() call; this keeps
+                    // the usage of RNG values from (most) Samplers that use
+                    // RNGs consistent, which improves reproducability /
+                    // debugging.
+                    if (!InsideExclusive(pixel, pixelBounds)) continue;
+
+                    do {
+                        // Initialize _CameraSample_ for current sample
+                        CameraSample cameraSample =
+                            tileSampler->GetCameraSample(pixel);
+
+                        // Generate camera ray for current sample
+                        RayDifferential ray;
+                        Float rayWeight =
+                            camera->GenerateRayDifferential(cameraSample, &ray);
+                        ray.ScaleDifferentials(
+                            1 / std::sqrt((Float)tileSampler->samplesPerPixel));
+                        ++nCameraRays;
+
+                        // Evaluate radiance along camera ray
+                        Spectrum L(0.f);
+                        if (rayWeight > 0)
+                            L = Li(ray, scene, *tileSampler, arena);
+
+                        // Issue warning if unexpected radiance value returned
+                        if (L.HasNaNs()) {
+                            LOG(ERROR) << StringPrintf(
+                                "Not-a-number radiance value returned "
+                                "for pixel (%d, %d), sample %d. Setting to "
+                                "black.",
+                                pixel.x, pixel.y,
+                                (int)tileSampler->CurrentSampleNumber());
+                            L = Spectrum(0.f);
+                        } else if (L.y() < -1e-5) {
+                            LOG(ERROR) << StringPrintf(
+                                "Negative luminance value, %f, returned "
+                                "for pixel (%d, %d), sample %d. Setting to "
+                                "black.",
+                                L.y(), pixel.x, pixel.y,
+                                (int)tileSampler->CurrentSampleNumber());
+                            L = Spectrum(0.f);
+                        } else if (std::isinf(L.y())) {
+                            LOG(ERROR) << StringPrintf(
+                                "Infinite luminance value returned "
+                                "for pixel (%d, %d), sample %d. Setting to "
+                                "black.",
+                                pixel.x, pixel.y,
+                                (int)tileSampler->CurrentSampleNumber());
+                            L = Spectrum(0.f);
+                        }
+                        VLOG(1) << "Camera sample: " << cameraSample
+                                << " -> ray: " << ray << " -> L = " << L;
+
+                        // Add camera ray's contribution to image
+                        filmTile->AddSample(cameraSample.pFilm, L, rayWeight);
+
+                        // Free _MemoryArena_ memory from computing image sample
+                        // value
+                        arena.Reset();
+                    } while (tileSampler->StartNextSample());
+                }
+                LOG(INFO) << "Finished image tile " << tileBounds;
+
+                // Merge image tile into _Film_
+                camera->film->MergeFilmTile(std::move(filmTile));
+                reporter.Update();
+            },
+            nTiles);
+        reporter.Done();
+    }
+    LOG(INFO) << "Rendering finished";
+
+    // Save final image after rendering
+    camera->film->WriteImage();
+}
+#endif
 
 Spectrum SamplerIntegrator::SpecularReflect(
     const RayDifferential &ray, const SurfaceInteraction &isect,
